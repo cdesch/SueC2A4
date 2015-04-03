a5fbbe1b7dedc865742f1addbb694bd7d21b223a
Chris Desch <cdesch@gmail.com>
2015-04-02 20:36:25 -0400

    Adding XCode Config
    Signed-off-by: Chris Desch <cdesch@gmail.com>

---
+++ SueC2L4/SueC2L4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SueC2L4
+//
+//  Created by cj on 4/2/15.
+//  Copyright (c) 2015 KickinEspresso. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
